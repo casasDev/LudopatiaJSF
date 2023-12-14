c4637699f2f53af836b8e591e17534f5fc741581
KaiserCarlos <carlos.vilarchao710@gmail.com>
2023-12-14 23:26:10 +0100

    pruebas y demas
    Signed-off-by: KaiserCarlos <carlos.vilarchao710@gmail.com>

--- src/main/java/modelo/bean/QueryQuestionBean.java
+++ src/main/java/modelo/bean/QueryQuestionBean.java
@@ -1,16 +1,27 @@
 package modelo.bean;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
+import javax.jdo.annotations.Queries;
+
+import businessLogic.*;
 
 import org.primefaces.event.SelectEvent;
+
+import domain.Event;
 
 public class QueryQuestionBean {
 
 	private Date fecha;
+	
+	private List<Event> eventos;
+	
+    private List<Queries> queries;
 	
 	public Date getFecha() {
 		return fecha;
@@ -20,15 +31,36 @@ public class QueryQuestionBean {
 		this.fecha = fecha;
 	}
 	
+	public List<Event> getListadoEvntos() {
+		
+		return eventos;
+		
+	}
+	
 	public void onDateSelect(SelectEvent event) {
 		FacesContext.getCurrentInstance().addMessage(null,
 		new FacesMessage("Fecha escogida: "+event.getObject()));
+		cargarEventos();
 	}
 	
 	public QueryQuestionBean() {
 		
 		
 	}
+	
+	private void cargarEventos() {
+      /*  if (fecha != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(fecha);
+
+            // Llama al servicio para obtener eventos de la base de datos
+            eventos = eventService.getEventsByDate(calendar.getTime());
+        } else {
+            // Manejo de error, la fecha es nula
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "La fecha seleccionada es nula."));
+        }*/
+    }
 	
 	
 	
