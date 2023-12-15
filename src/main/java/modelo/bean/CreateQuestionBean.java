eb777f21bb52f64a0f8d92e934074caeb5df710e
1001063 <1001063@u030056.gi.ehu.es>
2023-12-15 11:52:37 +0100

    chupito de semen

--- src/main/java/modelo/bean/CreateQuestionBean.java
+++ src/main/java/modelo/bean/CreateQuestionBean.java
@@ -1,8 +1,30 @@
 package modelo.bean;
 
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.SelectEvent;
+
+import antlr.debug.Event;
 import businessLogic.BLFacade;
 import gui.MainGUI;
 
 public class CreateQuestionBean {
-	
+	private Date fecha;
+
+	public CreateQuestionBean() {}
+
+	public Date getFecha() {
+		return this.fecha;
+	}
+
+	public void setFecha(Date d) {
+		this.fecha = d;
+	}
+
+	public void onDateSelect(SelectEvent e) {
+		FacesContext.getCurrentInstance().addMessage("", new FacesMessage("Events: " + e.getObject()));
+	}
 }
