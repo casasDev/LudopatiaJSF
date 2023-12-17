0e2929020559054c8089dd2cfe56f6033067b21d
KaiserCarlos <carlos.vilarchao710@gmail.com>
2023-12-17 13:19:15 +0100

    Supuestamente esta "hecho" lo de blfacade y demas pero a saber si esta bien
    Signed-off-by: KaiserCarlos <carlos.vilarchao710@gmail.com>

--- src/main/java/businessLogic/BLFacade.java
+++ src/main/java/businessLogic/BLFacade.java
@@ -3,18 +3,19 @@ package businessLogic;
 
 
 import dominio.Questions;
-<<<<<<< HEAD
 
+import java.util.Date;
 import java.util.Vector;
 
-=======
->>>>>>> branch 'master' of https://github.com/casasDev/LudopatiaJSF.git
+import javax.servlet.annotation.WebServlet;
+
 import dominio.Eventos;
 import exceptions.EventFinished;
 import exceptions.QuestionAlreadyExist;
 
-
-public class BLFacade {
+//@WebService
+@WebServlet
+public interface BLFacade {
 
 	/**
 	 * This method creates a question for an event, with a question text and the minimum bet
@@ -26,7 +27,8 @@ public class BLFacade {
 	 * @throws EventFinished if current data is after data of the event
  	 * @throws QuestionAlreadyExist if the same question already exists for the event
 	 */
-	@WebMethod Questions createQuestion(Eventos event, String question, float betMinimum) throws EventFinished, QuestionAlreadyExist;
+	//@WebMethod 
+	Questions createQuestion(Eventos event, String question, float betMinimum) throws EventFinished, QuestionAlreadyExist;
 	
 	
 	/**
@@ -35,7 +37,8 @@ public class BLFacade {
 	 * @param date in which events are retrieved
 	 * @return collection of events
 	 */
-	@WebMethod public Vector<Eventos> getEvents(Date date);
+	//@WebMethod 
+	public Vector<Eventos> getEvents(Date date);
 	
 	/**
 	 * This method retrieves from the database the dates a month for which there are events
@@ -43,13 +46,15 @@ public class BLFacade {
 	 * @param date of the month for which days with events want to be retrieved 
 	 * @return collection of dates
 	 */
-	@WebMethod public Vector<Date> getEventsMonth(Date date);
+	//@WebMethod 
+	public Vector<Date> getEventsMonth(Date date);
 	
 	/**
 	 * This method calls the data access to initialize the database with some events and questions.
 	 * It is invoked only when the option "initialize" is declared in the tag dataBaseOpenMode of resources/config.xml file
 	 */	
-	@WebMethod public void initializeBD();
+	//@WebMethod 
+	public void initializeBD();
 	
 	
 }
--- src/main/java/businessLogic/BLFacadeImplementation.java
+++ src/main/java/businessLogic/BLFacadeImplementation.java
@@ -1,6 +1,5 @@
 package businessLogic;
 
-<<<<<<< HEAD
 import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.Vector;
@@ -43,10 +42,10 @@ public class BLFacadeImplementation {
 	            da.initializeDB(); // Initialize database using Hibernate
 	            da.close();
 	        }
-	        dbManager = da;
+	        dbManager = (DataAccess) da;
 	    }
 
-	    @WebMethod
+	    //@WebMethod
 	    public Questions createQuestion(Eventos event, String question, float betMinimum)
 	            throws EventFinished, QuestionAlreadyExist {
 	        dbManager.open();
@@ -61,42 +60,38 @@ public class BLFacadeImplementation {
 
 	        return qry;
 	    }
+	    
 
-	    @WebMethod
+
+	   // @WebMethod
 	    public Vector<Eventos> getEvents(Date date) {
 	        dbManager.open();
 	        Vector<Eventos> events = dbManager.getEvents(date);
 	        dbManager.close();
 	        return events;
 	    }
+	    
 
-	    @WebMethod
+	   // @WebMethod
 	    public Vector<Date> getEventsMonth(Date date) {
 	        dbManager.open();
 	        Vector<Date> dates = dbManager.getEventsMonth(date);
 	        dbManager.close();
 	        return dates;
 	    }
-
+	    
 	    public void close() {
 	        dbManager.close();
 	    }
-
-	    @WebMethod
+	    
+	    //@WebMethod
 	    public void initializeBD() {
 	        dbManager.open();
 	        dbManager.initializeDB();
 	        dbManager.close();
 	    }
-=======
-import configuration.ConfigXML;
-import dataAccess.DataAccessInterface;
 
-public class BLFacadeImplementation {
-		
-	DataAccessInterface dbManager;
-
-	public BLFacadeImplementation()  {		
+/*	public BLFacadeImplementation()  {		
 		System.out.println("Creating BLFacadeImplementation instance");
 		ConfigXML c=ConfigXML.getInstance();
 		
@@ -106,10 +101,10 @@ public class BLFacadeImplementation {
 			dbManager.initializeDB();
 			dbManager.close();
 			}
-		*/
-	}
+		
+	}*/
 	
-    public BLFacadeImplementation(DataAccessInterface da)  {
+  /*  public BLFacadeImplementation(DataAccessInterface da)  {
 		
 		System.out.println("Creating BLFacadeImplementation instance with DataAccess parameter");
 		ConfigXML c=ConfigXML.getInstance();
@@ -122,86 +117,6 @@ public class BLFacadeImplementation {
 
 		}
 		dbManager=da;		
-	}
-	
-
-	/**
-	 * This method creates a question for an event, with a question text and the minimum bet
-	 * 
-	 * @param event to which question is added
-	 * @param question text of the question
-	 * @param betMinimum minimum quantity of the bet
-	 * @return the created question, or null, or an exception
-	 * @throws EventFinished if current data is after data of the event
- 	 * @throws QuestionAlreadyExist if the same question already exists for the event
-	 */
-   @WebMethod
-   public Question createQuestion(Event event, String question, float betMinimum) throws EventFinished, QuestionAlreadyExist{
-	   
-	    //The minimum bed must be greater than 0
-		dbManager.open();
-		Question qry=null;
-		
-	    
-		if(new Date().compareTo(event.getEventDate())>0)
-			throw new EventFinished(ResourceBundle.getBundle("Etiquetas").getString("ErrorEventHasFinished"));
-				
-		
-		 qry=dbManager.createQuestion(event,question,betMinimum);		
-
-		dbManager.close();
-		
-		return qry;
-   };
-	
-	/**
-	 * This method invokes the data access to retrieve the events of a given date 
-	 * 
-	 * @param date in which events are retrieved
-	 * @return collection of events
-	 */
-    @WebMethod	
-	public Vector<Event> getEvents(Date date)  {
-		dbManager.open();
-		Vector<Event>  events=dbManager.getEvents(date);
-		dbManager.close();
-		return events;
-	}
-
+	}*/
     
-	/**
-	 * This method invokes the data access to retrieve the dates a month for which there are events
-	 * 
-	 * @param date of the month for which days with events want to be retrieved 
-	 * @return collection of dates
-	 */
-	@WebMethod public Vector<Date> getEventsMonth(Date date) {
-		dbManager.open();
-		Vector<Date>  dates=dbManager.getEventsMonth(date);
-		dbManager.close();
-		return dates;
-	}
-	
-	
-	public void close() {
-		//DataAccess dB4oManager=new DataAccess(false);
-
-		//dB4oManager.close();
-		dbManager.close();
-
-
-	}
-
-	/**
-	 * This method invokes the data access to initialize the database with some events and questions.
-	 * It is invoked only when the option "initialize" is declared in the tag dataBaseOpenMode of resources/config.xml file
-	 */	
-    @WebMethod	
-	 public void initializeBD(){
-    	dbManager.open();
-		dbManager.initializeDB();
-		dbManager.close();
-	}
->>>>>>> branch 'master' of https://github.com/casasDev/LudopatiaJSF.git
-	
 }
