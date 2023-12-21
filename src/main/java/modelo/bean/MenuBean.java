9394ffdf56b8c29324d7d871db846cc474431e92
KaiserCarlos <carlos.vilarchao710@gmail.com>
2023-12-21 16:31:23 +0100

    menu
    Signed-off-by: KaiserCarlos <carlos.vilarchao710@gmail.com>

---
+++ src/main/java/modelo/bean/MenuBean.java
@@ -0,0 +1,16 @@
+package modelo.bean;
+
+public class MenuBean {
+
+	public String irVerPreg() {
+        //return "QueryQuestions.xhtml?faces-redirect=true";
+		return "irQueryQuestion";
+    }
+
+    public String irCrearPreg() {
+        //return "CreateQuestion.xhtml?faces-redirect=true";
+    	return "irCreateQuestion";
+    }
+	
+	
+}
